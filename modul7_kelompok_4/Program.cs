819eb8cc48e2abe738d280a14ab49871228314a4
ilhamridzuan <ilhamridzuaan@gmail.com>
2025-04-09 07:45:38 +0700

    menambahkan class DataMahasiswa103022300033

--- modul7_kelompok_4/Program.cs
+++ modul7_kelompok_4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        var data1 = new DataMahasiswa103022300033();
+        data1.ReadJSON();
+    }
+}
--- modul7_kelompok_4/obj/Debug/net8.0/modul7_kelompok_4.AssemblyInfo.cs
+++ modul7_kelompok_4/obj/Debug/net8.0/modul7_kelompok_4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul7_kelompok_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89829a83a4f57af583fb2fc1561e50db13db746d")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul7_kelompok_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul7_kelompok_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
