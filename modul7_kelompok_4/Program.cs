71c02a9df79e60671cff94cef5155d07ae3fbf0f
Avriela <avriela@example.com>
2025-04-09 07:39:43 +0700

    menambahkan class DataMahasiswa103022300066

--- modul7_kelompok_4/Program.cs
+++ modul7_kelompok_4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul7_kelompok_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dataMahasiswa = new DataMahasiswa103022300066();
+        dataMahasiswa.ReadJSON();
+    }
+}
